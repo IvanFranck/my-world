8ff537fb422f49a55231c283de6d9419acae7177
NZIMA Ivan <nzimaivan007@gmail.com>
2026-02-07 22:41:54 +0100

    Add HTTP client and token management functionality
    - Introduced HttpClient class for managing API requests with automatic token handling and refresh logic.
    - Implemented TokenManager for managing authentication tokens, including storage, retrieval, and expiration checks.
    - Added constants for API base URL and token key management.
    - Created authApiRoutes for defining authentication-related endpoints.

---
+++ apps/web/core/http/tokenManager.ts
@@ -0,0 +1,36 @@
+import { TOKEN_MANAGER_KEY } from "@/app/lib/constants";
+import { jwtDecode, JwtPayload } from "jwt-decode";
+
+export class TokenManager {
+  static getToken(): string | null {
+    return localStorage.getItem(TOKEN_MANAGER_KEY);
+  }
+
+  static setToken(token: string): void {
+    localStorage.setItem(TOKEN_MANAGER_KEY, token);
+  }
+
+  private static decodeToken(token: string): JwtPayload | null {
+    const result = jwtDecode(token);
+    if (typeof result === "object" && result !== null && "exp" in result) {
+      return result;
+    }
+    return null;
+  }
+
+  /**
+   * Check if the token is expired
+   * @param token - The token to check
+   * @param bufferTime - The buffer time in seconds
+   * @returns True if the token is expired, false otherwise
+   */
+  public static isTokenExpired(token: string, bufferTime: number = 0): boolean {
+    const decoded = this.decodeToken(token);
+    if (!decoded || !decoded.exp) return true;
+    return decoded.exp < Date.now() / 1000 - bufferTime;
+  }
+
+  static removeToken(): void {
+    localStorage.removeItem(TOKEN_MANAGER_KEY);
+  }
+}
---
+++ apps/web/services/auth/authApiRoutes.ts
@@ -0,0 +1,3 @@
+export const authApiRoutes = {
+  refreshToken: "/auth/refresh-token",
+};
